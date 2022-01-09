43d528c81191ab56ec75fb693baf3026c7b00950
Maxim Grigorev <maximgri93@gmail.com>
2022-01-09 18:42:15 +0300

    check commit

---
+++ src/WriteToFile.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class WriteToFile {
+    public static void main(String args[]) throws FileNotFoundException {
+        File file = new File("test");
+        PrintWriter pw = new PrintWriter(file);
+
+
+    }
+}
